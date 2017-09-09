078fb19d5e94d6c58be7bd36ae2235ccea0c2fb9
max-codeware <max.codeware@gmail.com>
2017-09-09 15:04:10 +0200

    Added some code and fixed some bug

--- FFtests.rb
+++ FFtests.rb
@@ -9,12 +9,12 @@ files.each do |file|
 end
 
 x = Function::Variable.new "x"
-y = Function::Variable.new "y"
-k = Function::Variable.new "k"
+# y = Function::Variable.new "y"
+# k = Function::Variable.new "k"
 
-n3 = Function::Number.new 3
-n4 = Function::Number.new 4
-n0 = Function::Number.new 0
+ n3 = Function::Number.new 3
+# n4 = Function::Number.new 4
+# n0 = Function::Number.new 0
 
 pi = Function::PI
 e  = Function::E
@@ -23,13 +23,21 @@ inf  = Function::P_Infinity
 minf = Function::M_Infinity
 
 sin = Function::Sin.new x*n3
-log = Function::Log.new(sin)/sin
+cos = Function::Cos.new n3*x
+#log = Function::Log.new(sin)/sin
+acos = Function::Acos.new n3*x
+asin = Function::Asin.new n3*x
+tan = Function::Tan.new n3*x
+atan = Function::Atan.new n3*x
+sqrt = Function::Sqrt.new n3*x
 
 #puts n3 * y * n4 * n3 *x * minf
-r = log
+r = cos
 #r = y * n3 + k * y 	  #/ n4 #* y * k * inf
 puts r
 
 dr = r.diff(x)
 
-puts "d(#{r.to_s})/dk = #{dr.to_s}"
+puts "d(#{r.to_s})/dx = #{dr.to_s}"
+
+

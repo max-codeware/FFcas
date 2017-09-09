33ea50bf9a7b11ee0d1f7842a57d2854d448d86d
max-codeware <max.codeware@gmail.com>
2017-09-09 17:31:18 +0200

    Added documentation

--- FFbinary_ops/FFdiv.rb
+++ FFbinary_ops/FFdiv.rb
@@ -5,6 +5,13 @@
 # License:: Distributed under MIT license
 module Function
 
+   
+  ##
+  # This class represents an abstract division between two algebric elements
+  #
+  #
+  # Author:: Massimiliano Dal Mas (mailto:max.codeware@gmail.com)
+  # License:: Distributed under MIT license
   class Div < BinaryOp
   
     alias :red :reduce
@@ -14,6 +21,17 @@ module Function
       super
     end
     
+    # * **argument**:
+    #   * Negative
+    #   * BinaryOp and children
+    #   * Variable
+    #   * Math_Funct
+    #   * Numeric
+    #   * P_Infinity_Val
+    #   * M_Infinity_Val
+    # * **returns**: 
+    #   * Children of BinaryOp
+    #   * +nil+ if the operation can't be performed
     def +(obj)
       return nil unless (self.top) || (self =~ obj) || (self == obj)
       if self == obj
@@ -39,6 +57,17 @@ module Function
       return Sum.new(self,obj)
     end
     
+    # * **argument**:
+    #   * Negative
+    #   * BinaryOp and children
+    #   * Variable
+    #   * Math_Funct
+    #   * Numeric
+    #   * P_Infinity_Val
+    #   * M_Infinity_Val
+    # * **returns**: 
+    #   * Children of BinaryOp
+    #   * +nil+ if the operation can't be performed
     def -(obj)
       return nil unless (self.top) || (self =~ obj) || (self == obj)
       return Number.new 0 if self == obj
@@ -59,6 +88,17 @@ module Function
       return Diff.new(self,obj).reduce
     end
     
+    # * **argument**:
+    #   * Negative
+    #   * BinaryOp and children
+    #   * Variable
+    #   * Math_Funct
+    #   * Numeric
+    #   * P_Infinity_Val
+    #   * M_Infinity_Val
+    # * **returns**: 
+    #   * Children of BinaryOp
+    #   * +nil+ if the operation can't be performed
     def *(obj)
       return nil unless (self.top) || (self =~ obj) || (self == obj) || (obj.is_a? Constant) || (obj.is_a? Number)
       return Pow.new(self,Number.new(2)) if self == obj
@@ -93,6 +133,17 @@ module Function
       return Prod.new(obj,self)
     end
     
+    # * **argument**:
+    #   * Negative
+    #   * BinaryOp and children
+    #   * Variable
+    #   * Math_Funct
+    #   * Numeric
+    #   * P_Infinity_Val
+    #   * M_Infinity_Val
+    # * **returns**: 
+    #   * Children of BinaryOp
+    #   * +nil+ if the operation can't be performed
     def /(obj)
       if obj.is_a? Div
         return self * Div.new(obj.right,obj.left).reduce
@@ -101,16 +152,34 @@ module Function
       end
     end
     
+    # * **argument**:
+    #   * Negative
+    #   * BinaryOp and children
+    #   * Variable
+    #   * Math_Funct
+    #   * Numeric
+    #   * P_Infinity_Val
+    #   * M_Infinity_Val
+    # * **returns**: 
+    #   * Pow
+    #   * +nil+ if the operation can't be performed
     def **(obj)
       return nil unless self.top
       return Pow.new(self,obj).reduce
     end
     
+    # Thells whether obj is similar so self. That is: x/n =~ y/n or x/n =~ x
     def =~(obj)
       return self.left == obj.left if obj.is_a? Div
       return self.left == obj
     end
     
+    # Simplifies the fraction
+    # * 0 / n   => 0
+    # * -x / y  => -(x/y)
+    # * x/(-x)  => -(x/y)
+    # * x / 1   => x
+    # * x / 0   => ∞
     def reduce
       self.red
       return Number.new 0 if self.left == 0
@@ -121,12 +190,20 @@ module Function
       return self
     end
     
+    # Inverts the sign of the class
+    #
+    # * **returns**: Negative
     def invert
       ret = Negative.new(self).reduce
       ret.top = self.top
       return ret
     end
     
+    # Performs the differential of a product:
+    # d(x/y) = (dx*y - x*dy) / y^2
+    #
+    # * **argument**: variable according to the differential must be done
+    # * **returns**: Children of BinaryOp or Function element
     def diff(var)
       lft, rht = self.left, self.right
       lft.top, rht.top = true, true
@@ -136,12 +213,14 @@ module Function
       return Div.new(d_lft - d_rht, rht).reduce
     end
     
+    # * **returns**: string representation of the class
     def to_s
       lft = (self.left.is_a? Sum or self.left.is_a? Diff) ? "(#{self.left.to_s})" : self.left.to_s
       rht = (self.right.is_a? Sum or self.right.is_a? Diff) ? "(#{self.right.to_s})" : self.right.to_s
       return "#{lft}/#{rht}"
     end
     
+    # * **returns**: string representation of the class for a block
     def to_b
       lft = (self.left.is_a? Sum or self.left.is_a? Diff) ? "(#{self.left.to_b})" : self.left.to_b
       lft = (self.right.is_a? Sum or self.right.is_a? Diff) ? "(#{self.right.to_b})" : self.right.to_b
